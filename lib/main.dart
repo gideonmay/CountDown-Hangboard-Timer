e6674b369c07868b7e72bf88b26109f87f17b745
iDoc1 <ianardocherty@gmail.com>
2023-02-12 20:21:35 -0800

    Created base app with bottom navigation

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'screens/workouts_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const AppScaffold());
+  }
+}
+
+class AppScaffold extends StatefulWidget {
+  const AppScaffold({super.key});
+
+  @override
+  State<AppScaffold> createState() => _AppScaffoldState();
+}
+
+class _AppScaffoldState extends State<AppScaffold> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screenOptions = <Widget>[
+    Text('Index 0: My Workouts'),
+    Text('Index 1: Timer'),
+    Text('Index 2: Settings')
+  ];
+
+  static const List<Widget> _titleOptions = <Widget>[
+    Text('My Workouts'),
+    Text('Timer'),
+    Text('Settings')
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _titleOptions.elementAt(_selectedIndex),
+      ),
+      body: Center(
+        child: _screenOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'My Workouts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: 'Timer',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}

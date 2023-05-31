6b64753ebe2de98def616b659a2a18a80a549449
Ian Docherty <iandocherty@MacBook-Pro.local>
2023-05-30 20:03:44 -0700

    Added tests for GripDTO

---
+++ test/models/grip_dto_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:countdown_app/models/grip_dto.dart';
+
+void main() {
+  test('GripDTO.standard() has correct standard values', () {
+    final gripDTO = GripDTO.standard();
+
+    expect(gripDTO.sets, 1);
+    expect(gripDTO.reps, 1);
+    expect(gripDTO.workSeconds, 10);
+    expect(gripDTO.restSeconds, 5);
+    expect(gripDTO.breakMinutes, 0);
+    expect(gripDTO.breakSeconds, 30);
+    expect(gripDTO.lastBreakMinutes, 0);
+    expect(gripDTO.lastBreakSeconds, 30);
+    expect(gripDTO.edgeSize, null);
+    expect(gripDTO.gripTypeID, null);
+  });
+
+  test('GripDTO.standard() returns correct last break duration', () {
+    final gripDTO = GripDTO.standard();
+
+    expect(gripDTO.lastBreakDuration, const Duration(seconds: 30));
+  });
+}
